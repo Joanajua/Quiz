b7682879142fe18028209f631acafdc17aca5936
Joana Boiso-Ayan <J.Ayan@ucas.ac.uk>
2021-01-25 15:03:46 +0000

    Claims part took out

--- Quiz1/Quiz1/Controllers/AdministrationController.cs
+++ Quiz1/Quiz1/Controllers/AdministrationController.cs
@@ -1,13 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using Quiz1.Models.Identity;
 using Quiz1.ViewModels.Identity;
 
 namespace Quiz1.Controllers
@@ -305,9 +303,6 @@ namespace Quiz1.Controllers
                 return NotFound($"User with Id = {id} cannot be found");
             }
 
-            // GetClaimsAsync retunrs the list of user Claims
-            //var userClaims = await _userManager.GetClaimsAsync(user);
-
             // GetRolesAsync returns the list of user Roles
             var userRoles = await _userManager.GetRolesAsync(user);
 
@@ -316,7 +311,6 @@ namespace Quiz1.Controllers
                 Id = user.Id,
                 Email = user.Email,
                 UserName = user.UserName,
-                //Claims = userClaims.Select(c => c.Value).ToList(),
                 Roles = userRoles
             };
 
@@ -435,87 +429,6 @@ namespace Quiz1.Controllers
 
             return RedirectToAction("EditUser", new { Id = userId });
         }
-
-        //[HttpGet]
-        //public async Task<IActionResult> ManageUserClaims(string userId)
-        //{
-        //    if (userId == null)
-        //    {
-        //        ViewBag.ErrorMessage = $"User Id is not in the correct format";
-        //        return BadRequest();
-        //    }
-
-        //    var user = await _userManager.FindByIdAsync(userId);
-        //    var claims = ClaimsStore.AllClaims.ToList();
-
-        //    if (user == null)
-        //    {
-        //        ViewBag.ErrorMessage = $"User with Id = {userId} cannot be found";
-        //        return NotFound($"User with Id = {userId} cannot be found");
-        //    }
-
-        //    // UserManager service GetClaimsAsync method gets all the current claims of the user
-        //    var existingUserClaims = await _userManager.GetClaimsAsync(user);
-
-        //    var model = new UserClaimsViewModel
-        //    {
-        //        UserId = userId
-        //    };
-
-            // Loop through each claim we have in our application
-        //    foreach (Claim claim in claims)
-        //    {
-        //        UserClaim userClaim = new UserClaim
-        //        {
-        //            ClaimType = claim.Type
-        //        };
-
-        //        // If the user has the claim, set IsSelected property to true, so the checkbox
-        //        // next to the claim is checked on the UI
-        //        if (existingUserClaims.Any(c => c.Type == claim.Type))
-        //        {
-        //            userClaim.IsSelected = true;
-        //        }
-
-        //        model.Claims.Add(userClaim);
-        //    }
-
-        //    return View(model);
-        //}
-
-        //[HttpPost]
-        //public async Task<IActionResult> ManageUserClaims(UserClaimsViewModel model)
-        //{
-        //    var user = await _userManager.FindByIdAsync(model.UserId);
-
-        //    if (user == null)
-        //    {
-        //        ViewBag.ErrorMessage = $"User with Id = {model.UserId} cannot be found.";
-        //        return NotFound($"User with Id = {model.UserId} cannot be found.");
-        //    }
-
-        //    // Get all the user existing claims and delete them.
-        //    var claims = await _userManager.GetClaimsAsync(user);
-        //    var result = await _userManager.RemoveClaimsAsync(user, claims);
-
-        //    if (!result.Succeeded)
-        //    {
-        //        ModelState.AddModelError("", "Cannot remove user existing claims.");
-        //        return View(model);
-        //    }
-
-        //    // Add all the claims that are selected on the UI.
-        //    result = await _userManager.AddClaimsAsync(user,
-        //        model.Claims.Where(c => c.IsSelected).Select(c => new Claim(c.ClaimType, c.ClaimType)));
-
-        //    if (!result.Succeeded)
-        //    {
-        //        ModelState.AddModelError("", "Cannot add selected claims to user.");
-        //        return View(model);
-        //    }
-
-        //    return RedirectToAction("EditUser", new { Id = model.UserId });
-        //}
 
         [HttpPost]
         public async Task<IActionResult> DeleteUser(string id)
--- Quiz1/Quiz1/Models/Identity/UserClaim.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Quiz1.Models.Identity
-{
-    public class UserClaim
-    {
-        public string ClaimType { get; set; }
-        public bool IsSelected { get; set; }
-    }
-}
